9bd2d3c36a104792611511f29d01a58f95afe051
Andrei <46579508+Andreisgl@users.noreply.github.com>
2022-12-14 17:50:42 -0300

    Update MMF_main.ino

--- MMF_main/MMF_main.ino
+++ MMF_main/MMF_main.ino
@@ -34,18 +34,38 @@ void loop()
     else // If it does have a master command
   {
     //Keep parsing command.
-    String command = AParser.acode_parse_command(data, 0); // Parse first command.
+    String command = AParser.acode_parse_command(data, 0); // Parse command.
+    int command_number;
+    String parameters = AParser.acode_parse_command(data, 1); // Parse parameters.
     String aux = command;
-    Serial.print("Code Command: ");
+
+    Serial.print("Command: ");
     Serial.print( command ); //Get command
 
     Serial.print("\tFirst letter: ");
-    Serial.print( command[0] ); //Get command
+    Serial.print( command[0] );
+
+    Serial.print("\tCommand number: ");
+    aux.remove(0,aux.indexOf(aux[0]) + 1);
+    command_number = aux.toInt();
+    Serial.print(command_number);
+
+
+
+    Serial.print("\tParameters: ");
+    Serial.print( parameters ); //Get command
+
+    //Serial.println();
+
+
+    
+
 
     switch (command[0])
     {
       case 'A': // A-code "A" is for movement
       Serial.println("\t!");
+      
         break;
     }
   }
