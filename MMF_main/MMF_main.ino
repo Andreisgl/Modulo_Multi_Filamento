a96167bbe293fa88689ee1c8ad305e206fd15ef2
Andrei <46579508+Andreisgl@users.noreply.github.com>
2022-12-14 16:05:11 -0300

    Update MMF_main.ino

--- MMF_main/MMF_main.ino
+++ MMF_main/MMF_main.ino
@@ -19,15 +19,29 @@ void loop()
   if(Serial.available())
   {
     String incoming = Serial.readString();
-    Serial.print("Return: ");
     String data = serial_check_master_command(incoming);
     if(data == "\\") //If it has not a master command:
     {
-      //Send to other serial port (printer).
+      data = incoming;
+      //Send to other serial port (printer). TODO
     }
     else // If it does have a master command
     {
       //Keep parsing command.
+      String command = acode_parse_command(data, 0); // Parse first command.
+      String aux = command;
+      char aux2[20];
+      aux.toCharArray(aux2, 20);
+      Serial.print("Full command: ");
+      Serial.println( command ); //Get command
+      aux.remove(0);
+      switch (int(aux2))
+      {
+        case int('A'):
+          Serial.println("Parsed command: ");
+          Serial.print(aux2);
+          break;
+      }
     }
   }
 
@@ -36,17 +50,28 @@ void loop()
 String serial_check_master_command(String incoming)
 {
   String data = incoming;
-    data.remove(data.indexOf((incoming[3]))+1);
-    if(data == m_command) // If master command is recognized...
-    {
-      data = incoming; // Parse data that comes after it.
-      data.remove(0, data.indexOf((incoming[3])) + 1);
-      return data;
-    }
-    else // Or else...
-    {
-      //Send data to other serial port. TODO!!!!!!!!
-      return "\\";
-    }
+  data.remove(data.indexOf((incoming[3]))+1);
+  if(data == m_command) // If master command is recognized...
+  {
+    data = incoming; // Parse data that comes after it.
+    data.remove(0, data.indexOf((incoming[3])) + 1);
+    return data;
+  }
+  else // Or else...
+  {
+    //Send data to other serial port. TODO!!!!!!!!
+    return "\\";
+  }
     
+}
+
+String acode_parse_command(String input, int pos)
+{
+  String aux1 = input;
+  String aux2 = input;
+  aux1.remove(aux1.indexOf(" ")); //Separate from first space forward.
+  aux2.remove(0, aux2.indexOf(" "));
+
+  String output[] = {aux1, aux2};
+  return output[pos];
 }
