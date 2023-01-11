2b79d01fa8000a217c22f5d91c8766cf78d0762b
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-01-11 19:04:09 -0300

    move class to own file

--- MMF_main/main.py
+++ MMF_main/main.py
@@ -1,44 +1,7 @@
 import utime
 import time
 
-class Motor:
-    dir_pin = ""
-    step_pin = ""
-    steps_unit = ""
-    def __init__(self, d, s, u):
-        global dir_pin
-        global step_pin
-        global steps_unit
-        self.d = d
-        self.s = s
-        self.u = u
-        
-        step_pin = machine.Pin(s, machine.Pin.OUT)
-        dir_pin = machine.Pin(d, machine.Pin.OUT)
-        steps_unit = u
-        
-        step_pin.value(0)
-        dir_pin.value(0)
-        print("!!!")
-        
-    def step_stepper(self, direction):        
-        dir_pin.value(direction)
-        step_pin.value(0)
-        step_pin.value(1)
-   
-    def move_motor(self, dist_unit, vel_UpS, direction):
-        print("###")
-        
-        total_steps = dist_unit * steps_unit
-        steps_psec = vel_UpS * steps_unit
-        tim = total_steps/steps_psec
-        #
-        sec_pstep = tim/total_steps
-        
-        ###
-        for i in range(total_steps):
-            self.step_stepper(direction)
-            time.sleep(sec_pstep)
+import
 
 
 def command_parser(inc):
@@ -54,8 +17,11 @@ def mmf_move(args):
             motorA.move_motor(int(args[1]), 1, 0)
 
 
+from machine import Pin, PWM
 motorA = Motor(3, 2, 93) #Motor for filament A
-
+selector_AB = PWM(Pin(6))
+selector_AB.freq(50)
+selector_AB.duty_u16(0)
 
    
         
@@ -69,7 +35,7 @@ master_command = "MMF:"
 #while True:
 #    uart1.write('hello\n')
 
-while True:
+while False:
     incoming = uart1.read()
     incoming = str(incoming)
     if incoming != 'None': #Only continue if something was received
@@ -92,9 +58,12 @@ while True:
             
         else: #If no master command is present, pass command to printer:
             pass #TODO: Implement pass to printer
+    
     uart1.write('\n')
     time.sleep(0.1)
-    
+
+print("set servo")
+selector_AB.duty_u16(65535)
     
 
 
